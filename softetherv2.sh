57dd3c78004d8e0b6b83a1d5b8b2d0a54e54502b
TheGoose <nikilarocks2@hotmail.com.au>
2018-05-08 17:26:16 +1000

    Add files via upload

--- softetherv2.sh
+++ softetherv2.sh
@@ -3,6 +3,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install build-essential make git openssl libreadline-dev libssl-dev nano libncurses5-dev libncursesw5-dev lib32ncurses5-dev -y
 git clone https://github.com/SoftEtherVPN/SoftEtherVPN_Stable.git
 cd SoftEtherVPN_Stable/
+./configure
 make
 make install
 vpnserver start
